558cdee709fdcb00db407b4764b6ba3320327d0d
Michael Hahn <mh@slack-corp.com>
2017-10-04 21:30:31 -0700

    Remove auth sections

--- components/password.js
+++
@@ -1,8 +0,0 @@
-import { withStyles } from "material-ui/styles";
-import TextField from "material-ui/TextField";
-
-const styles = {};
-
-const Password = ({ classes }) => <TextField label="password" />;
-
-export default withStyles(styles)(Password);
