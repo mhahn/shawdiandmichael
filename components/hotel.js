71346564bfe658a8d097c651d4921df6d6097707
Michael Hahn <mh@slack-corp.com>
2017-11-30 19:49:21 -0800

    Make hotels linkable

--- components/hotel.js
+++ components/hotel.js
@@ -1,43 +1,52 @@
 import { withStyles } from "material-ui/styles";
 import Typography from "material-ui/Typography";
+import Grid from "material-ui/Grid";
 
 const styles = theme => ({
   root: {
-    margin: 30,
+    padding: 30,
     display: "flex",
     flexWrap: "wrap",
     justifyContent: "center",
-    height: 530
+    cursor: "pointer",
+    "&:hover": {
+      backgroundColor: "#f4f4f4"
+    }
   },
   img: {
     objectFit: "cover",
     width: "100%",
-    height: "auto"
+    height: "100%"
   },
   imgContainer: {
     display: "flex",
-    alignItems: "flex-start"
+    alignItems: "flex-start",
+    width: "100%",
+    height: 250
   },
   textContainer: {
     display: "flex",
     flexDirection: "column",
-    width: "100%"
+    width: "100%",
+    paddingTop: 30
   }
 });
 
-const Hotel = ({ classes, name, children, img: imgName }) => (
-  <div className={classes.root}>
-    <div className={classes.imgContainer}>
-      <img className={classes.img} src={`/assets/img/${imgName}`} />
+const Hotel = ({ classes, name, children, img: imgName, href }) => (
+  <Grid item sm={6} xs={12}>
+    <div className={classes.root} onClick={() => window.open(href, "_blank")}>
+      <div className={classes.imgContainer}>
+        <img className={classes.img} src={`/assets/img/${imgName}`} />
+      </div>
+      <div className={classes.textContainer}>
+        <Typography type="title" align="center">
+          {name}
+        </Typography>
+        <br />
+        <Typography align="center">{children}</Typography>
+      </div>
     </div>
-    <div className={classes.textContainer}>
-      <Typography type="title" align="center">
-        {name}
-      </Typography>
-      <br />
-      <Typography align="center">{children}</Typography>
-    </div>
-  </div>
+  </Grid>
 );
 
 export default withStyles(styles)(Hotel);
--- pages/where-to-stay.js
+++ pages/where-to-stay.js
@@ -8,9 +8,6 @@ import Template from "../components/template";
 import Hotel from "../components/hotel";
 
 const styles = theme => ({
-  image: {
-    width: 630
-  },
   content: {
     maxWidth: 1000
   }
@@ -18,36 +15,43 @@ const styles = theme => ({
 
 const WhereToStay = ({ classes, pathname }) => (
   <Template title="Shawdi & Michael: Where to Stay" pathname={pathname}>
-    <Grid container className={classes.content} justify="center" align="center">
-      <Grid item xs={12} sm={6}>
-        <Hotel name="The Lodge at Sonoma" img="the-lodge.jpg">
-          This is the hotel Michael & Shawdi will be staying at! It's near
-          Sonoma Square (with frequent shuttles back and forth) and has a great
-          pool for us all to hang and enjoy a few drinks by!
-        </Hotel>
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <Hotel name="El Dorado Hotel" img="el-dorado.jpg">
-          This is a boutique hotel right on Sonoma Square! It’s perfect for
-          exploring the square and all the shops and amazing restaurants around
-          it.
-        </Hotel>
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <Hotel name="Best Western" img="best-western.jpg">
-          This is a hotel just around the corner from Sonoma Square. It’s a
-          great option if you’re planning to do lots of exploring and wine
-          tasting and just want a place to put your bags and rest your head.
-        </Hotel>
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <Hotel name="Airbnb" img="airbnb.jpg">
-          Of course, Airbnb is one of our favorite options when traveling! If
-          you’ve never stayed at an Airbnb before, you can use{" "}
-          <a href="http://www.airbnb.com/c/silbagian1">this link</a> for $40
-          off!
-        </Hotel>
-      </Grid>
+    <Grid className={classes.content} container justify="center">
+      <Hotel
+        name="The Lodge at Sonoma"
+        img="the-lodge.jpg"
+        href="http://www.marriott.com/hotels/travel/sfols-the-lodge-at-sonoma-renaissance-resort-and-spa/"
+      >
+        This is the hotel Michael & Shawdi will be staying at! It's near Sonoma
+        Square (with frequent shuttles back and forth) and has a great pool for
+        us all to hang and enjoy a few drinks by!
+      </Hotel>
+      <Hotel
+        name="El Dorado Hotel"
+        img="el-dorado.jpg"
+        href="https://www.eldoradosonoma.com/"
+      >
+        This is a boutique hotel right on Sonoma Square! It’s perfect for
+        exploring the square and all the shops and amazing restaurants around
+        it.
+      </Hotel>
+      <Hotel
+        name="Best Western"
+        img="best-western.jpg"
+        href="http://www.sonomavalleyinn.com/"
+      >
+        This is a hotel just around the corner from Sonoma Square. It’s a great
+        option if you’re planning to do lots of exploring and wine tasting and
+        just want a place to put your bags and rest your head.
+      </Hotel>
+      <Hotel
+        name="Airbnb"
+        img="airbnb.jpg"
+        href="http://www.airbnb.com/c/silbagian1"
+      >
+        Of course, Airbnb is one of our favorite options when traveling! If
+        you’ve never stayed at an Airbnb before, you can use{" "}
+        <a href="http://www.airbnb.com/c/silbagian1">this link</a> for $40 off!
+      </Hotel>
     </Grid>
   </Template>
 );
