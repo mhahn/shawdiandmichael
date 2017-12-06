d3cb6bca1ef2094de67efa660a8db2744dc816ef
Michael Hahn <mh@slack-corp.com>
2017-12-05 21:41:34 -0800

    Add GA

--- components/template.js
+++ components/template.js
@@ -31,7 +31,22 @@ const Template = ({ title, classes, children, pathname }) => (
   <div className={classes.root}>
     <Head>
       <title>{title}</title>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5CDML8P');`
+        }}
+      />
     </Head>
+    <noscript
+      dangerouslySetInnerHTML={{
+        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5CDML8P"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+      }}
+    />
     <div className={classes.content}>
       <HeaderTitle />
       <HeaderNav pathname={pathname} />
